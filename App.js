37ec6811a8f6195fd8a926e4b02de6c6483edea8
Joaquin-Parrilla <joaquin.p.olivera@gmail.com>
2020-08-07 21:53:08 -0300

    modifico index y creo JS para interactuar con tabla

--- App.js
+++ App.js
@@ -1,63 +1,18 @@
 /**
- * Coronavirus-live App
- * Version 1.0
- * Only map, popups & markers
+ * Coronavirus-live App.js
+ * Version 1.1
+ * Show data in html.table element
+ * By Joaquin-Parrilla
  */
 
+console.log(getData());
 
-const $mapa = document.querySelector('#mapa');
-
-const map = new window.google.maps.Map($mapa, {
-    center: {
-        lat: 0,
-        lng: 0
-    },
-    zoom: 3
-});
-renderData();
 
 async function getData(){
     const response = fetch('https://wuhan-coronavirus-api.laeyoung.endpoint.ainize.ai/jhu-edu/latest');
-    //const data = await response.json(); errors??
     const data = await (await response).json();
     return data;
 }
 
-const popup = new window.google.maps.InfoWindow(); // only
-
-function renderExtraData({ confirmed, deaths, recovered, provincestate, countryregion }) {
-    const coursed = confirmed - (deaths + recovered);
-    return `
-    <div>
-        <p><strong> ${provincestate} - ${countryregion} </strong></p>
-        <p>Confirmados: ${confirmed} </p>
-        <p>Recuperados: ${recovered} </p>
-        <p>Muertes: ${deaths} </p>
-        <p>Cursando: ${coursed} </p>
-    </div>
-    `;
-}
-
-async function renderData(){
-    const data = await getData();
-    console.log(data);
-    // loop array and put red markers in map:
-    data.forEach(item => {
-        // solo mostramos markers de paises que tengan casos confirmados:
-        if(item.confirmed >= 0){
-            const marker = new window.google.maps.Marker({
-                position: {
-                    lat: item.location.lat,
-                    lng: item.location.lat,
-                },
-                map
-            })    
-            marker.addListener('click', () => {
-                popup.setContent(renderExtraData())
-            });
-            popup.open(map, marker);
-        }
-    });
-}
 
 
